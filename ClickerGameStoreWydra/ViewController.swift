2074f13d2d2ee34faeb6bf3d25a2742b40ce728d
Brenden Wydra <brendenwwydra@gmail.com>
2023-10-03 09:49:54 -0500

    initial commit

--- ClickerGameStoreWydra/ViewController.swift
+++ ClickerGameStoreWydra/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ponyCount: UILabel!
+    
+    var ponies = 0
+    var poniesPerClick = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func updatePonies(){
+        ponyCount.text = "Ponies: \(ponies)"
+    }
 
 
+    @IBAction func newPonyPress(_ sender: UIButton) {
+        ponies += poniesPerClick
+        updatePonies()
+    }
+    
+    performSegue(withIdentifier identifier: String, sender: Any?) {
+        <#code#>
+    }
 }
 
---
+++ ClickerGameStoreWydra/ViewControllerStore.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerStore.swift
+//  ClickerGameStoreWydra
+//
+//  Created by BRENDEN WYDRA on 10/3/23.
+//
+
+import UIKit
+
+class ViewControllerStore: UIViewController {
+    
+    var ponies: Int!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
